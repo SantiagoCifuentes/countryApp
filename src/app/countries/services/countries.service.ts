3ce2985b89270a5ea7f4f7d4a5cf2f22b307ef5f
Santiago C <santiago.cifuentes@sofka.com.co>
2023-11-10 14:56:54 -0500

    country page terminado

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -13,6 +13,17 @@ export class CountriesService {
 
   constructor(private http: HttpClient) { }
 
+  searchCountryByAlphaCode(code: string): Observable<Country | null>{
+
+    const url = `${this.apiUrl}/alpha/${code}`
+
+
+    return this.http.get<Country[]>(url).pipe(
+      map(countries => countries.length > 0 ? countries[0] : null ),
+      catchError(error => of(null)))
+
+  }
+
   searchCapital(term: string): Observable<Country[]> {
 
     const url = `${this.apiUrl}/capital/${term}`
